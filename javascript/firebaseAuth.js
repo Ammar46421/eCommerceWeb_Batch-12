e4d2ce69989b436c6d69ddc48db27d6bc85d9d80
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2025-03-13 20:56:47 +0500

    Update firebaseAuth.js

--- javascript/firebaseAuth.js
+++ javascript/firebaseAuth.js
@@ -41,8 +41,8 @@
         const user=userCredential.user;
         const userData={
             email: email,
-            firstName: firstName,
-            lastName:lastName
+            firstName: firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase(),
+            lastName:lastName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase(),
         };
         showMessage('Account Created Successfully', 'signUpMessage');
         const docRef=doc(db, "users", user.uid);
